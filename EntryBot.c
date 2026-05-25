66dcb39647cc9e68d6ab93c2ec77a01cc62b2bdc
kushagar-lab <kushagardhiman2008@gmail.com>
2026-05-25 15:41:46 +0530

    EntryBot who checks age and Id

--- EntryBot.c
+++ EntryBot.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
- int age = 18;
- char hasId;
- printf("enter your age = ");
- scanf("%d",&age);
-   printf("do you have Id: (yes/no):");
-   scanf(" %c",&hasId);
-   if (age >= 18 && hasId == 'y'){
-    printf("entry allowed\n"); 
-   }  else if (age < 18 && hasId == 'y'){
-         printf("entry denied: Too young\n");
-         } else if (age >= 18 && hasId == 'n' ){
-            printf("hehehe sorry no id: no entry: ");
-         } else {
-            printf ("entry denied! no Id and underage!");
-         }
-       system ("pause");
-return 0;
-}
+int age = 18;
+char hasId;
+  printf("enter your age = ");
+  scanf("%d",&age);
+ 
+  printf("do you have Id: (yes/no):");
+  scanf(" %c",&hasId);
+ 
+  if (age >= 18 && hasId == 'y'){
+  printf("entry allowed\n"); 
+    }  
+  else if (age < 18 && hasId == 'y'){
+   printf("entry denied: Too young\n");
+     } 
+  else if (age >= 18 && hasId == 'n' ){
+   printf("hehehe sorry no id: no entry: ");
+     }
+  else {
+   printf ("entry denied! no Id and underage!");
+      }
+  system ("pause");
+  return 0;
+     }
 
